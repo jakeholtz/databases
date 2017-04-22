dd42f4c6f3e070d825d64db2d5bfb7596f5e1563
Hack Reactor Student <student@hackreactor.com>
2017-04-21 19:30:11 -0700

    Created database tables

--- server/db/index.js
+++ server/db/index.js
@@ -5,3 +5,19 @@ var mysql = require('mysql');
 // and to the database "chat".
 
 
+
+// mysql -u root -p
+
+// CREATE database ___
+
+// USE ___
+
+// 
+// CREATE TABLE <table-name> (
+//   <name-of-column-1> <data-type-of-column> [ADDITIONAL-INFO-ABOUT-THIS-COLUMN],
+//   <name-of-column-2> <data-type-of-column> [ADDITIONAL-INFO-ABOUT-THIS-COLUMN],
+//                               ...
+//   <name-of-column-n> <data-type-of-column> [ADDITIONAL-INFO-ABOUT-THIS-COLUMN],
+//   [OTHER-SCHEMA-DEFINITION-COMMANDS]
+// );
+
--- server/schema.sql
+++ server/schema.sql
@@ -1,10 +1,33 @@
 CREATE DATABASE chat;
-
 USE chat;
 
-CREATE TABLE messages (
-  /* Describe your table here.*/
+
+CREATE TABLE rooms (
+  id MEDIUMINT NOT NULL AUTO_INCREMENT,
+  rm_name VARCHAR(50),
+  PRIMARY KEY(id)
 );
+
+
+CREATE TABLE users (
+  id MEDIUMINT NOT NULL AUTO_INCREMENT,
+  name VARCHAR(32),
+  PRIMARY KEY(id)
+);
+
+
+CREATE TABLE messages (
+  id MEDIUMINT NOT NULL AUTO_INCREMENT,
+  msg VARCHAR(255), 
+  room_id MEDIUMINT,
+  created MEDIUMINT, 
+  user_id MEDIUMINT,
+  PRIMARY KEY(id),
+  FOREIGN KEY(room_id) REFERENCES rooms(id),
+  FOREIGN KEY(user_id) REFERENCES users(id) 
+);
+
+
 
 /* Create other tables and define schemas for them here! */
 
